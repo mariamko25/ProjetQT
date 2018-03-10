81ba734bfbaa6476d47134e053175ac31eae4139
mitchOr <mngatimo@yahoo.com>
2018-03-10 19:16:46 +0100

    ajout de la classe bd

---
+++ c_init_bd.cpp
@@ -0,0 +1,6 @@
+#include "c_init_bd.h"
+
+c_init_bd::c_init_bd()
+{
+
+}
---
+++ c_init_bd.h
@@ -0,0 +1,11 @@
+#ifndef C_INIT_BD_H
+#define C_INIT_BD_H
+
+
+class c_init_bd
+{
+public:
+    c_init_bd();
+};
+
+#endif // C_INIT_BD_H
